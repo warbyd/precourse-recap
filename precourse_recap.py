8bbab923f16dcf41b9554c0ff2f2a33000c50122
user <user@192.168.1.115>
2023-02-27 19:25:22 +0000

    precourse recap

---
+++ precourse_recap.py
@@ -0,0 +1,16 @@
+# create variables of different types
+day_of_week = "Monday"
+current_week = 1
+current_day_of_week = 1
+
+# call the print function using the variables and some operators
+# we also use the str() function to convert integers to strings
+
+print("Today is " + day_of_week + ", Week " + str(current_week) 
+      + " Day " + str(current_day_of_week) + " of CodeClan")
+
+# run your file in your terminal:
+# python3 precourse_recap.py
+#
+# and it should print out:
+# Today is Monday, Week 1 Day 1 of CodeClan
